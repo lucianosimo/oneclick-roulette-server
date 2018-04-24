13043c2562e904943248cba54bce42a3e12d96b6
Luciano Simo <lucianosimo87@gmail.com>
2018-04-24 14:58:50 -0300

    Removed innecesaries varaiables

--- index.js
+++ index.js
@@ -18,22 +18,15 @@ var api = new ParseServer({
   appName: process.env.APP_NAME || '',
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || '8vMdncG7MgLbcrKTNiaoTUzV',
+  masterKey: process.env.MASTER_KEY || 'aVjrZbmrYA4dVQRHZBQW4EGR', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  customPages: {
-    invalidLink: invalidLinkTemplateUrl,
-    verifyEmailSuccess: verifyEmailSuccessTemplateUrl,
-    choosePassword: choosePasswordTemplateUrl,
-    passwordResetSuccess: passwordResetSuccessTemplateUrl
-  },
   // Enable email verification
   verifyUserEmails: false
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
